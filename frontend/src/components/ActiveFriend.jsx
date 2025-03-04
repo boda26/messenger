056821998bbc63c88adf5910d75f2ee12ef63515
boda26 <sbd20010626@outlook.com>
2025-03-04 14:56:17 -0800

    home page

---
+++ frontend/src/components/ActiveFriend.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function ActiveFriend() {
+    return (
+		<div className="active-friend">
+			<div className="image-active-icon">
+				<div className="image">
+					<img src="/image/aaa.webp" />
+					<div className="active-icon">
+					</div>
+				</div>
+
+				<div className="image">
+					<img src="/image/aaa.webp" />
+					<div className="active-icon">
+					</div>
+				</div>
+
+				<div className="image">
+					<img src="/image/aaa.webp" />
+					<div className="active-icon">
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ActiveFriend;
---
+++ frontend/src/components/Friends.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Friends() {
+    return (
+        <div className="friend">
+            <div className="friend-image">
+                <div className="image">
+					<img src="/image/aaa.webp" />
+				</div>
+            </div>
+
+			<div className="friend-name-seen">
+				<div className="friend-name">
+					<h4>Jack</h4>
+				</div>
+			</div>
+        </div>
+    );
+}
+
+export default Friends;
